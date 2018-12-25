6f48c2bde9e4dd90cefcca948f83a021e026fdbd
GuoqiangSun <2481288068@qq.com>
2018-12-25 11:36:23 +0800

    bit增加判断 0 和 1

--- app/src/main/java/cn/com/common/test/main/MainActivity.java
+++ app/src/main/java/cn/com/common/test/main/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import cn.com.common.test.R;
+import cn.com.common.test.shake.ShakeActivity;
 import cn.com.common.test.global.FileManager;
 import cn.com.common.test.p2p.p2pAndroid.P2pClientActivity;
 import cn.com.common.test.scanOR.ScanORCodeActivity;
@@ -22,6 +23,62 @@ import cn.com.swain169.log.TFlog;
 import cn.com.swain169.log.Tlog;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private void testTFLog() {
+        TFlog.v("TFlog", " test Tlog v");
+        TFlog.v("TFlog", " test Tlog v", new Throwable());
+        TFlog.v("TFlog", new Throwable());
+
+        TFlog.d("TFlog", " test Tlog d");
+        TFlog.d("TFlog", new Throwable());
+        TFlog.d("TFlog", " test Tlog d", new Throwable());
+
+        TFlog.i("TFlog", " test Tlog i");
+        TFlog.i("TFlog", new Throwable());
+        TFlog.i("TFlog", " test Tlog i", new Throwable());
+
+        TFlog.w("TFlog", " test Tlog w");
+        TFlog.w("TFlog", new Throwable());
+        TFlog.w("TFlog", " test Tlog w", new Throwable());
+
+        TFlog.e("TFlog", " test Tlog e");
+        TFlog.e("TFlog", new Throwable());
+        TFlog.e("TFlog", " test Tlog e", new Throwable());
+
+        TFlog.a("TFlog", " test Tlog a");
+        TFlog.a("TFlog", new Throwable());
+        TFlog.a("TFlog", " test Tlog a", new Throwable());
+
+    }
+
+    private void testLog() {
+        Tlog.v("swain", " test Tlog v");
+        Tlog.v("swain", " test Tlog v", new Throwable());
+        Tlog.v("swain", new Throwable());
+
+        Tlog.d("swain", " test Tlog d");
+        Tlog.d("swain", new Throwable());
+        Tlog.d("swain", " test Tlog d", new Throwable());
+
+        Tlog.i("swain", " test Tlog i");
+        Tlog.i("swain", new Throwable());
+        Tlog.i("swain", " test Tlog i", new Throwable());
+
+        Tlog.w("swain", " test Tlog w");
+        Tlog.w("swain", new Throwable());
+        Tlog.w("swain", " test Tlog w", new Throwable());
+
+        Tlog.e("swain", " test Tlog e");
+        Tlog.e("swain", new Throwable());
+        Tlog.e("swain", " test Tlog e", new Throwable());
+
+        Tlog.a("swain", " test Tlog a");
+        Tlog.a("swain", new Throwable());
+        Tlog.a("swain", " test Tlog a", new Throwable());
+
+    }
+
     private PermissionRequest mPermissionRequest;
 
     @Override
@@ -99,58 +156,8 @@ public class MainActivity extends AppCompatActivity {
         startActivity(new Intent(this, P2pClientActivity.class));
     }
 
-    private void testTFLog() {
-        TFlog.v("TFlog", " test Tlog v");
-        TFlog.v("TFlog", " test Tlog v", new Throwable());
-        TFlog.v("TFlog", new Throwable());
-
-        TFlog.d("TFlog", " test Tlog d");
-        TFlog.d("TFlog", new Throwable());
-        TFlog.d("TFlog", " test Tlog d", new Throwable());
-
-        TFlog.i("TFlog", " test Tlog i");
-        TFlog.i("TFlog", new Throwable());
-        TFlog.i("TFlog", " test Tlog i", new Throwable());
-
-        TFlog.w("TFlog", " test Tlog w");
-        TFlog.w("TFlog", new Throwable());
-        TFlog.w("TFlog", " test Tlog w", new Throwable());
-
-        TFlog.e("TFlog", " test Tlog e");
-        TFlog.e("TFlog", new Throwable());
-        TFlog.e("TFlog", " test Tlog e", new Throwable());
-
-        TFlog.a("TFlog", " test Tlog a");
-        TFlog.a("TFlog", new Throwable());
-        TFlog.a("TFlog", " test Tlog a", new Throwable());
-
-    }
-
-    private void testLog() {
-        Tlog.v("swain", " test Tlog v");
-        Tlog.v("swain", " test Tlog v", new Throwable());
-        Tlog.v("swain", new Throwable());
-
-        Tlog.d("swain", " test Tlog d");
-        Tlog.d("swain", new Throwable());
-        Tlog.d("swain", " test Tlog d", new Throwable());
-
-        Tlog.i("swain", " test Tlog i");
-        Tlog.i("swain", new Throwable());
-        Tlog.i("swain", " test Tlog i", new Throwable());
-
-        Tlog.w("swain", " test Tlog w");
-        Tlog.w("swain", new Throwable());
-        Tlog.w("swain", " test Tlog w", new Throwable());
-
-        Tlog.e("swain", " test Tlog e");
-        Tlog.e("swain", new Throwable());
-        Tlog.e("swain", " test Tlog e", new Throwable());
-
-        Tlog.a("swain", " test Tlog a");
-        Tlog.a("swain", new Throwable());
-        Tlog.a("swain", " test Tlog a", new Throwable());
-
+    public void shake(View view) {
+        startActivity(new Intent(this, ShakeActivity.class));
     }
 
 }
